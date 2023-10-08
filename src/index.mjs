d07448981efabe9bcb45305a3808491f69e74112
ktomo <dreamguidance1@gmail.com>
2023-10-08 04:24:47 +0000

    feature: TODO 追加ボタン

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,46 @@
-import "./styles.css";
+// import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+function onClickComplete() {
+  console.log("@@@@@@");
+  alert("complete");
+}
+
+function onClickDelete() {
+  alert("delete");
+}
+
+function createInCompleteTodo(inputText) {
+  // TODO要素作成
+  const li = document.createElement("li");
+  const p = document.createElement("p");
+  p.innerText = inputText;
+  const buttonComplete = document.createElement("button");
+  buttonComplete.innerText = "完了";
+  buttonComplete.addEventListener("click", () => onClickComplete());
+  const buttonDelete = document.createElement("button");
+  buttonDelete.innerText = "削除";
+  buttonDelete.addEventListener("click", () => onClickDelete());
+
+  const div = document.createElement("div");
+  div.className = "list-row";
+  div.appendChild(p);
+  div.appendChild(buttonComplete);
+  div.appendChild(buttonDelete);
+  li.appendChild(div);
+
+  const ul = document.getElementById("incomplete-list");
+  ul.appendChild(li);
+
+  // ボタンにイベントを設定する
+}
+
+const onClickAdd = () => {
+  // TODO値を取得し初期化
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  createInCompleteTodo(inputText);
+};
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
